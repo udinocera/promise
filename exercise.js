03a504586ca04f493ec6ca75a10df29527416cb2
udinocera <udinocera@gmail.com>
2023-09-09 23:55:23 +0200

    fine

--- exercise.js
+++ exercise.js
@@ -1,1 +1,15 @@
 const number = 15;
+const checkNumber = (number) => {
+    return new Promise((resolve, reject) => {
+        if (number >= 10) {
+            resolve(`${number} is greater than or equal to 10`);
+        } else {
+            reject(`${number} isn't greater 10`)
+        }
+    });
+};
+
+checkNumber(number)
+.then((val) => console.log(val))
+.catch((err) => console.error(err))
+
